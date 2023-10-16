64094d670586a50106e8b4a830891bc434406470
John Spinella <jrspinella@users.noreply.github.com>
2023-10-16 10:05:48 -0400

    Update to examples

---
+++ examples/Government/add_data_connector/main.tf
@@ -0,0 +1,10 @@
+
+
+module "mod_sentinel_connector" {
+  #source = "azurenoops/overlays-sentinel/azurerm//modules/data_connectors/asc"  
+  #version = "x.x.x"  
+  source   = "../../../modules/data_connectors/asc"
+  depends_on = [ azurerm_log_analytics_workspace.sentinel_workspace,azurerm_log_analytics_solution.solutions ]
+  
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.sentinel_workspace.id  
+}
