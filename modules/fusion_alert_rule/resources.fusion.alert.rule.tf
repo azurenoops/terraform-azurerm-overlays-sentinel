e9bfe80942811cc531bb4e0052865682b49c9ee2
John Spinella <jrspinella@users.noreply.github.com>
2023-10-16 12:34:32 -0400

    Updates to module file

--- modules/fusion_alert_rule/resources.fusion.alert.rule.tf
+++ modules/fusion_alert_rule/resources.fusion.alert.rule.tf
@@ -5,8 +5,9 @@
 # DEPLOY AZURE SENTINEL SCHEDULED ALERT RULES ##
 ################################################
 
-resource "azurerm_sentinel_alert_rule_fusion" "example" {
+resource "azurerm_sentinel_alert_rule_fusion" "sentinel_alert_rule_fusion" {
   name                       = var.name
   log_analytics_workspace_id = var.log_analytics_workspace_id
   alert_rule_template_guid   = var.alert_rule_template_guid
+  enabled                    = var.enable_rule_alert
 }
