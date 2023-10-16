3e2d62eac0da13658ccf058948b3fcee96143b04
John Spinella <jrspinella@users.noreply.github.com>
2023-10-16 16:15:09 -0400

    Update Outputs

--- modules/fusion_alert_rule/outputs.tf
+++ modules/fusion_alert_rule/outputs.tf
@@ -1,2 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
+
+output "id" {
+  description = "The ID of the alert rule."
+  value       = azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusion.id
+}
--- modules/ms_security_incident_alert_rule/outputs.tf
+++ modules/ms_security_incident_alert_rule/outputs.tf
@@ -1,2 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
+
+output "id" {
+  description = "The ID of the alert rule."
+  value       = azurerm_sentinel_alert_rule_ms_security_incident.sentinel_alert_rule_ms_security_incident.id
+}
