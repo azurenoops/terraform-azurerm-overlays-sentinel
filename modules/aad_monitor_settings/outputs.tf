3c4c87c4391debdb99d295c8daf31d81d3712b19
John Spinella <jrspinella@users.noreply.github.com>
2024-02-01 14:06:23 -0500

    Fix errors

--- modules/aad_monitor_settings/outputs.tf
+++ modules/aad_monitor_settings/outputs.tf
@@ -2,17 +2,17 @@
 # Licensed under the MIT License.
 
 output "id" {
-  value = azurerm_monitor_aad_diagnostic_setting.aad_diagnostic_settings.id
+  value = azurerm_monitor_aad_diagnostic_setting.aad_logs.id
 }
 
 output "name" {
-  value = azurerm_monitor_aad_diagnostic_setting.aad_diagnostic_settings.name
+  value = azurerm_monitor_aad_diagnostic_setting.aad_logs.name
 }
 
 output "log_analytics_workspace_id" {
-  value = azurerm_monitor_aad_diagnostic_setting.aad_diagnostic_settings.log_analytics_workspace_id
+  value = azurerm_monitor_aad_diagnostic_setting.aad_logs.log_analytics_workspace_id
 }
 
 output "storage_account_id" {
-  value = azurerm_monitor_aad_diagnostic_setting.aad_diagnostic_settings.storage_account_id
+  value = azurerm_monitor_aad_diagnostic_setting.aad_logs.storage_account_id
 }
