0bc71930e520f914ae2843950fd02bae91b94554
John Spinella <jrspinella@users.noreply.github.com>
2024-02-06 12:41:31 -0500

    remove retention

--- variables.sentinel.aad.settings.tf
+++ variables.sentinel.aad.settings.tf
@@ -4,17 +4,11 @@
 variable "data_connector_aad_enabled" {
   description = "Whether the Azure Active Directory logs are retrieved."
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "data_connector_aad_logs" {
   description = "List of Azure Active Directory log category."
   type        = list(string)
   default     = ["AuditLogs", "SignInLogs", "NonInteractiveUserSignInLogs", "ServicePrincipalSignInLogs", "ManagedIdentitySignInLogs", "ProvisioningLogs", "ADFSSignInLogs", "RiskyUsers", "UserRiskEvents", "NetworkAccessTrafficLogs", "RiskyServicePrincipals", "ServicePrincipalRiskEvents", "EnrichedOffice365AuditLogs", "MicrosoftGraphActivityLogs"]
-}
-
-variable "retention_policy_days" {
-  description = "Number of days to retain the logs. Default is 1 day."
-  type        = number
-  default     = 1  
 }
