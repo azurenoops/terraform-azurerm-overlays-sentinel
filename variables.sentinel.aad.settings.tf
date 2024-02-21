889579b184001d716bee6c8d2336bac8106c87b7
John Spinella <jrspinella@users.noreply.github.com>
2024-02-21 13:12:58 -0500

    disabled add dia settings and remove role assignment

--- variables.sentinel.aad.settings.tf
+++ variables.sentinel.aad.settings.tf
@@ -1,14 +1,14 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-variable "data_connector_aad_enabled" {
+/* variable "data_connector_aad_enabled" {
   description = "Whether the Azure Active Directory logs are retrieved."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "data_connector_aad_logs" {
   description = "List of Azure Active Directory log category."
   type        = list(string)
   default     = ["AuditLogs", "SignInLogs", "NonInteractiveUserSignInLogs", "ServicePrincipalSignInLogs", "ManagedIdentitySignInLogs", "ProvisioningLogs", "ADFSSignInLogs", "RiskyUsers", "UserRiskEvents", "NetworkAccessTrafficLogs", "RiskyServicePrincipals", "ServicePrincipalRiskEvents", "EnrichedOffice365AuditLogs", "MicrosoftGraphActivityLogs"]
-}
+} */
